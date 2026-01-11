ad97184bb255f418fe6df103a0135807af6ef1fa
Zyphon12342 <shivansh12342@gmail.com>
2026-01-11 16:53:13 +0530

    Containerized billing-service + exposed gRPC port(9001)

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,33 @@
+package com.pm.billingservice.grpc;
+
+
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("createBillingAccount request received {}", billingRequest.toString());
+
+        // Business logic for example save to db or perform arithmetic
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        // Used to send a response from gRCP service to client
+        responseObserver.onNext(response);
+
+        // response is completed and ending current cycle of responses
+        responseObserver.onCompleted();
+    }
+}
