cb0443b9f090c099a5ce28aee6382ac5e272e25c
Zyphon12342 <shivansh12342@gmail.com>
2026-01-16 04:57:18 +0530

    Added validateToken for auth-service

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,11 +1,14 @@
 package com.pm.authservice.util;
 
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
@@ -31,5 +34,17 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 *10)) // 10 hours
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+        } catch (SignatureException e) {
+            throw new JwtException("Invalid JWT signature");
+        } catch (JwtException e) {
+            throw new JwtException("Invalid JWT");
+        }
     }
 }
